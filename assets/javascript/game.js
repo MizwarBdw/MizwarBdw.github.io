07b4c9ccbe5bcf55fc314109cacac3b4549ef7d2
MizwarBdw <mizwar.badawi@gmail.com>
2020-12-18 14:51:34 +0700

    wadidaw

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+function playAudio(url){
+    new Audio(url).play();
+}
+
+var mainGamePage = document.querySelector('.gamePage')
+var enemy = document.querySelector('.enemys')
+
+function randomPos(){
+    playAudio('./assets/audio/laser.mp3')
+
+    enemy.style.top = `${Math.floor(Math.random(300*50) * 600)}px`;
+    enemy.style.left = `${Math.floor(Math.random(175*125) * 950)}px`;
+
+    var score = parseInt(document.getElementById('h2').innerHTML)
+    score += 10
+    document.getElementById('h2').innerHTML = score
+}
+
